d1d655afa9f3e5aa9051d64010da72d7b652c031
Code Addict <eamon5174@gmail.com>
2026-03-12 11:23:55 +0530

    Simplify addition by reading input directly
    Removed command-line argument handling and directly read input from user.

--- go-python-integration/add.py
+++ go-python-integration/add.py
@@ -1,17 +1,4 @@
-import sys
+a = int(input())
+b = int(input())
 
-def main():
-    if len(sys.argv) != 3:
-        print("Error: Two numbers required")
-        sys.exit(1)
-
-    try:
-        a = int(sys.argv[1])
-        b = int(sys.argv[2])
-        print(a + b)   # Only prints the result (important for Go)
-    except ValueError:
-        print("Error: Invalid numbers")
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+print(a + b)
