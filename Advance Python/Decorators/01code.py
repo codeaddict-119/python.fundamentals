8b5cb0d926efa588bc5add87680bebee4ce41fb8
Eamon <eamon112009@gmail.com>
2026-03-16 07:45:41 +0530

    added Advance python

--- Advance Python/Decorators/01code.py
+++ Advance Python/Decorators/01code.py
@@ -1,3 +1,6 @@
+''' @my_decorator is syntactic sugar for say_hello = my_decorator(say_hello). 
+It modifies the behavior of say_hello() by wrapping it inside wrapper(). 
+The wrapper function adds behavior before and after the original function call.'''
 def my_decorator(func):
     def wrapper():
         print("Something is happening before the function is called.")
